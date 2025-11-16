4e7ba3cae47f6b045ae1456bbaca02358d044cdc
babyblue64 <abhay.raveendran2000@gmail.com>
2025-11-16 18:45:52 +0530

    Initial commit

---
+++ src/controllers/auth.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from 'express';
+import { prismaClient } from '..';
+import { hashSync } from 'bcrypt';
+
+export const signup = async (req: Request, res: Response) => {
+    const { email, password, name } = req.body;
+
+    let user = await prismaClient.user.findFirst({ where: { email: email } });
+
+    if (user) {
+        throw Error('User already exists!');
+    };
+
+    user = await prismaClient.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: hashSync(password, 10)
+        },
+    });
+    res.json(user);
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from 'express';
+import { PORT } from './secrets';
+import rootRouter from './routes';
+import { PrismaClient } from '@prisma/client';
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use('/', rootRouter);
+
+export const prismaClient = new PrismaClient();
+
+app.listen(PORT);
