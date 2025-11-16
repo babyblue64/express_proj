d461ee1d768e47ebc557f5e867145b500b2ce9fe
babyblue64 <abhay.raveendran2000@gmail.com>
2025-11-16 19:12:27 +0530

    feat: add login endpoint

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from 'express';
 import { prismaClient } from '..';
-import { hashSync } from 'bcrypt';
+import { hashSync, compareSync } from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../secrets';
 
 export const signup = async (req: Request, res: Response) => {
     const { email, password, name } = req.body;
@@ -19,4 +21,24 @@ export const signup = async (req: Request, res: Response) => {
         },
     });
     res.json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    let user = await prismaClient.user.findFirst({ where: { email: email } });
+
+    if (!user) {
+        throw Error('User not registered');
+    };
+
+    if (!compareSync(password, user.password)) {
+        throw Error('Incorrect password');
+    };
+
+    const token = jwt.sign({ 
+        userId: user.id
+    }, JWT_SECRET)
+
+    res.json(token);
 };
